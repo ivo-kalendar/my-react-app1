a0f615347b5297dc4099b803775f03e96d09ae83
Ivo <ivokalendar@icloud.com>
2020-04-20 13:03:06 +0200

    Refactoring...

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,16 +1,22 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { ConnectedTaskList } from './TaskList'
 
-const mapStateToProps = state => {return { groups: state.groups }}
+const mapStateToProps = state => ({ groups: state.groups })
 
 const Dashboard = ({groups}) => (
-	<Fragment>
+	<div className="dashboard">
 		<h2>Dashboard!!!</h2>
-		{ groups.map( group => (
-			<ConnectedTaskList key={group.id} id={group.id} name={group.name} />
-		) )}
-	</Fragment>
+		{ 
+			groups.map( group => (
+				<ConnectedTaskList 
+					key={group.id} 
+					id={group.id} 
+					name={group.name} 
+				/>
+			)) 
+		}
+	</div>
 )
 
 export const ConnectedDashboard = connect(mapStateToProps) (Dashboard)
--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const mapStateToProps = (state, ownProps) => {
-	let groupID = ownProps.id
-	return {
-		name: ownProps.name,
-		id: groupID,
-		tasks: state.tasks.filter(task => task.group === groupID)
+const mapStateToProps = (state, groups) => (
+	{
+		name: groups.name,
+		tasks: state.tasks.filter(task => task.group === groups.id)
 	}
-}
+)
 
 const TaskList = ({tasks, name}) => (
-	<div>
+	<div className="tasklist">
 		<h3>{name}</h3>
 		<div>
-			{tasks.map(task => <div key={task.id}>{task.name}</div>)}
+			{tasks.map(task => <p key={task.id}>{task.name}</p>)}
 		</div>
 	</div>
 )
