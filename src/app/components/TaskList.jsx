c868fd79bb638c4b2d563bc563dadb8e33abc16a
Ivo <ivokalendar@icloud.com>
2020-04-21 22:52:51 +0200

    Completed nee task and routing

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { requestTaskCreation } from '../store/mutations'
 
-const mapStateToProps = (state, groups) => (
-	{
-		name: groups.name,
-		tasks: state.tasks.filter(task => task.group === groups.id)
+const mapStateToProps = (state, ownProps) => {
+	let groupID = ownProps.id
+	return {
+		name: ownProps.name,
+		id: groupID,
+		tasks: state.tasks.filter(task => task.group === groupID)
 	}
-)
+}
 
-const TaskList = ({tasks, name}) => (
+const mapDispatchToProps = (dispatch, ownProps) => {
+	return {
+		createNewTask(id) {	
+			dispatch(requestTaskCreation(id))
+		}
+	}
+}
+
+const TaskList = ({tasks, name, id, createNewTask}) => (
 	<div className="tasklist">
 		<h3>{name}</h3>
 		<div>
 			{tasks.map(task => <p key={task.id}>{task.name}</p>)}
 		</div>
+		<button onClick={() => createNewTask(id)}>Add New</button>
 	</div>
 )
 
-export const ConnectedTaskList = connect(mapStateToProps) (TaskList)
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps) (TaskList)
