10d2732b08feaacf046fd820568733c07cd67733
Ivo <ivokalendar@icloud.com>
2020-05-06 21:04:29 +0200

    Completed Route Guard...

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,21 +2,33 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../store/index'
 import { ConnectedDashboard } from './Dashboard'
+import { ConnectedLogin } from './Login'
 import { Router, Route } from 'react-router-dom'
 import { history } from '../store/history'
 import { ConnectedNavigation } from './Navigation'
 import { ConnectTaskDetail } from './TaskDetail'
+import { Redirect } from 'react-router'
+
+const RouteGuard = Component => ({ match }) => {
+	console.info("Route Guard ", match)
+	if (!store.getState().session.authenticated) {
+		return <Redirect to="/"/>
+	} {
+		return <Component match={match}/>
+	}
+}
 
 export const Main = () => (
 	<Router history={history}>
 		<Provider store={store}>
 			<div>
 				<ConnectedNavigation />
+				<Route exact path="/" component={ConnectedLogin} />
 				<Route exact path="/dashboard" 
-					render={()=>(<ConnectedDashboard />)} 
+					render={RouteGuard(ConnectedDashboard)} 
 				/>
 				<Route exact path="/task/:id" 
-					render={({match})=>(<ConnectTaskDetail match={match} />)} 
+					render={RouteGuard(ConnectTaskDetail)} 
 				/>
 			</div>
 		</Provider>
